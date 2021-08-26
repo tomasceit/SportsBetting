2dcc3d92322cc9992772f8933a36407b9e4e4ad1
Tomas Ceit <tomi.ceit@gmail.com>
2021-08-26 00:22:51 -0300

    diseno bootstrap

--- js/functions/writeFutbolGames.js
+++ js/functions/writeFutbolGames.js
@@ -3,16 +3,22 @@ let acumulador;
 const writeFutbolGames = (futbolGamesArray) => {
     let padreFutbol = document.getElementById("futbolGames");
     for (const game of futbolGamesArray) {
-        acumulador = document.createElement("div");
-        acumulador.innerHTML = `<div class="d-flex justify-content-around align-items-center">
-                                <span>Fecha</span>
-                                <div class="d-flex flex-column text-center" style="width: 200px">
-                                    <span>${game.team1}</span>
-                                    <span>${game.team2}</span>
-                                </div>
-                                <button class="btn btn-dark" type="button" style="width: 70px">${game.odds1}</button>
-                                <button class="btn btn-dark" type="button" style="width: 70px">${game.odds2}</button>
-                            </div>`;
+        acumulador = document.createElement("tr");
+        acumulador.innerHTML = `<td>
+                                    <div class="d-flex flex-column text-center">
+                                        <span>DIA/MES/AÑO</span>
+                                        <span>HORA:MIN</span>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="d-flex flex-column text-center" style="width: 220px">
+                                        <span>${game.team1}</span>
+                                        <span>${game.team2}</span>
+                                    </div>
+                                </td>
+                                <td><button class="btn btn-dark" type="button" style="width: 70px">${game.odds1}</button></td>
+                                <td><button class="btn btn-dark" type="button" style="width: 70px">DRAW</button></td>
+                                <td><button class="btn btn-dark" type="button" style="width: 70px">${game.odds2}</button></td>`;
         padreFutbol.appendChild(acumulador);
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,33 +1,3 @@
-// let mayoriaEdad = checkAge();
-// if (mayoriaEdad) {
-//     alert(`El equipo 1 es: ${game1.team1} y paga ${game1.odds1}`);
-//     alert(`El equipo 2 es: ${game1.team2} y paga ${game1.odds2}`);
-//     let teamBet = Number(prompt('¿Quieres apostar al equipo 1 o 2?'));
-//     // Declaro variables que luego voy a usar dentro del if
-//     let betTeam1 = true;
-//     let winAmount;
-
-//     if ((teamBet !== 1) && (teamBet !== 2)) {
-//         alert('No elegiste correctamente. Volvé a ingresar a la página para apostar.');
-//     } else {
-//         if (teamBet === 2) betTeam1 = false;
-//         let betAmount = Number(prompt('¿Cuanto queres apostar?'));
-//         if (betTeam1 && (betAmount > 0)) {
-//             // Llamo al metodo
-//             winAmount = game1.bet(betAmount, game1.odds1);
-//             alert(`Tus ganancias potenciales son de $${winAmount}`);
-//         } else if ((betTeam1 === false) && (betAmount > 0)) {
-//             winAmount = game1.bet(betAmount, game1.odds2);
-//             alert(`Tus ganancias potenciales son de $${winAmount}`);
-//         } else {
-//             alert('La cantidad apostada no es valida.');
-//         }
-//     }
-// } else {
-//     alert('Los menores de edad no tienen permitido apostar.');
-// }
-
-
 // Declaro el array que contiene todos los partidos
 const gamesArray = [];
 // Lleno el array - En un futuro lo hare con la API
@@ -44,3 +14,35 @@ const basketGamesArray = gamesArray.filter(game => game.sport.toLowerCase() === 
 writeFutbolGames(futbolGamesArray);
 writeBasketGames(basketGamesArray);
 writeTenisGames(tenisGamesArray);
+
+
+let mayoriaEdad = checkAge();
+// if (mayoriaEdad) {
+//     alert(`El equipo 1 es: ${game1.team1} y paga ${game1.odds1}`);    
+//     alert(`El equipo 2 es: ${game1.team2} y paga ${game1.odds2}`);
+//     let teamBet = Number(prompt('¿Quieres apostar al equipo 1 o 2?'));
+//     // Declaro variables que luego voy a usar dentro del if
+//     let betTeam1 = true;
+//     let winAmount;
+
+//     if ((teamBet !== 1) && (teamBet !== 2)) {
+//         alert('No elegiste correctamente. Volvé a ingresar a la página para apostar.');    
+//     } else {
+//         if (teamBet === 2) betTeam1 = false;    
+//         let betAmount = Number(prompt('¿Cuanto queres apostar?'));
+//         if (betTeam1 && (betAmount > 0)) {
+//             // Llamo al metodo    
+//             winAmount = game1.bet(betAmount, game1.odds1);
+//             alert(`Tus ganancias potenciales son de $${winAmount}`);
+//         } else if ((betTeam1 === false) && (betAmount > 0)) {
+//             winAmount = game1.bet(betAmount, game1.odds2);    
+//             alert(`Tus ganancias potenciales son de $${winAmount}`);
+//         } else {
+//             alert('La cantidad apostada no es valida.');    
+//         }
+//     }
+// } else {
+//     alert('Los menores de edad no tienen permitido apostar.');    
+// }
+
+
